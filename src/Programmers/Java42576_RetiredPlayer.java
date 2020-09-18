3458aece9cbb337383ec630780e67dbf2c02c703
lotty02cho <lotty02cho@hanmail.net>
2020-09-18 10:26:30 +0900

    first commit

---
+++ src/Programmers/Java42576_RetiredPlayer.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+public class Java42576_RetiredPlayer {
+
+    static String[] participant = {"leo", "kiki", "eden"};
+    static String[] completion = {"eden", "kiki"};
+
+    public static void main(String[] args){
+        System.out.println("hello Java42576_RetiredPlayer");
+        // https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
+
+        solution(participant, completion);
+
+
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        String answer ="";
+
+        return answer;
+    }
+}
