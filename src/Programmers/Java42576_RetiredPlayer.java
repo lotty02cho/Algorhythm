3debd7b3e0cd0b907e5d510225d2d3855703938a
lotty02cho <lotty02cho@hanmail.net>
2020-09-18 11:48:52 +0900

    20200918 프로그래머스(42576, java) 완주하지 못한 선수 완료

--- src/Programmers/Java42576_RetiredPlayer.java
+++ src/Programmers/Java42576_RetiredPlayer.java
@@ -1,22 +1,47 @@
 package Programmers;
 
+import java.util.*;
+
 public class Java42576_RetiredPlayer {
 
     static String[] participant = {"leo", "kiki", "eden"};
     static String[] completion = {"eden", "kiki"};
 
     public static void main(String[] args){
-        System.out.println("hello Java42576_RetiredPlayer");
-        // https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
+        System.out.println("Java42576_RetiredPlayer");
+        /*
+            프로그래머스
+            문제: 완주하지 못한 선수
+            https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
+         */
 
-        solution(participant, completion);
+
+        System.out.println(solution(participant, completion));
 
 
     }
 
     public static String solution(String[] participant, String[] completion) {
-        String answer ="";
 
+        String answer="";
+
+        // 정렬
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i = 0;
+
+        // for문 돌리며 중복되는 값이 하나도 나오지 않았다면, 그 값이 곧 정답
+        for(i = 0; i < completion.length; i ++){
+            if( !participant[i].equals(completion[i]) ){
+                answer = participant[i];
+                return answer;
+            }
+        }
+
+        // 돌았는데, 나머지가 있다면 그 값이 곧 정답
+        answer = participant[i];
         return answer;
+
     }
 }
