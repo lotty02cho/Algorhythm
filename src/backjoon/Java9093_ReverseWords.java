74c9d45c7a7fe7ed9c86980fbb709fe65fc00fc7
lotty02cho <lotty02cho@hanmail.net>
2020-09-26 16:38:55 +0900

    200926 단어뒤집기 문제 풀이

---
+++ src/backjoon/Java9093_ReverseWords.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Java9093_ReverseWords {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int k = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < k ; i++){
+            String[] input = br.readLine().split(" ");
+//            System.out.println(Arrays.toString(input));
+            for(int j=0;j<input.length;j++)
+            {
+                for(int m=input[j].length()-1; m>=0; m--)
+                {
+                    bw.write(input[j].charAt(m));
+                }
+                bw.write(" ");
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+        return;
+    }
+
+
+}
